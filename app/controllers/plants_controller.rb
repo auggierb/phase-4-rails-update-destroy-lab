b06e5fa4cd8d923b4e007c4c0ffa7ea54c58b88a
August <auggierutter@gmail.com>
2022-04-12 16:59:28 -0400

    commit tree bd288b6700856c9ad6c8323d314db913810d9137

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,9 +17,25 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end 
+  
+  def destroy
+  plant = find_plant
+  plant.delete
+  head :no_content
+  end
+
 
   private
 
+  def find_plant
+  Plant.find_by(id:params[:id])
+  
+  end
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
